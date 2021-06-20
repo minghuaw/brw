e4e09e4e7cf413f3fbc863aab8ad7394e1f1cd5e
minghuaw <michael.wu1107@gmail.com>
2021-06-19 17:39:50 -0700

    added unit test for writer

--- src/writer.rs
+++ src/writer.rs
@@ -40,3 +40,56 @@ pub trait Writer: Sized {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use async_trait::async_trait;
+
+    use crate::Writer;
+    // Simply print out receive items
+    struct TestWriter { }
+
+    #[derive(Debug)]
+    enum TestWriterItem {
+        Foo(String),
+        Bar(i32)
+    }
+
+    #[async_trait]
+    impl super::Writer for TestWriter {
+        type Item = TestWriterItem;
+        type Ok = ();
+        type Error = std::io::Error;
+
+        async fn op(&mut self, item: Self::Item) -> Option<Result<Self::Ok, Self::Error>> {
+            println!("{:?}", item);
+
+            Some(Ok(()))
+        }
+
+        async fn handle_result(res: Result<Self::Ok, Self::Error>) -> Option<()> {
+            if let Err(err) = res {
+                println!("{:?}", err);
+            }
+            Some(())
+        }
+    }
+
+    #[tokio::test]
+    async fn test_writer() {
+        let w = TestWriter{ };
+        let (tx, rx) = flume::unbounded();
+
+        let handle = tokio::task::spawn(w.writer_loop(rx.into_stream()));
+        
+        for i in 0..10 {
+            tx.send(TestWriterItem::Foo(i.to_string()))
+                .unwrap();
+            tx.send(TestWriterItem::Bar(i))
+                .unwrap();
+        }
+        drop(tx);
+
+        handle.await.unwrap();
+    }
+}
